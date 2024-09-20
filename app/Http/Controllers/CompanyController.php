8b272828fc6dc395aee287ee3d0abc1bbb0bac52
@parou.char <curlycode@outlook.com>
2024-09-20 09:58:35 +0300

    setting routes for companies

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Company;
 
 class CompanyController extends Controller
 {
-    //
+    public function index()
+    {
+        $companies = Company::latest()->get();
+        return view('main',['companies'=>$companies]);        
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
 
+//Routes for company
 
+Route::get('company',[CompanyController::class, 'index']);
+Route::get('company/{company:id}',[CompanyController::class, 'show']);
+Route::post('company',[CompanyController::class, 'store']);
+Route::post('company/{company:id}',[CompanyController::class, 'update']);
+Route::post('company/{company:id}', [CompanyController::class, 'destroy ']);
 
 
 Route::get('/dashboard', function () {
